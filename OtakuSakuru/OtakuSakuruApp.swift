61d1209f7a7522397c36fde81d0ee82e85cfa946
GregDeveaux <deveauxgreg@gmail.com>
2023-02-03 00:38:56 +0100

    add Gitignore

---
+++ OtakuSakuru/OtakuSakuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuSakuruApp.swift
+//  OtakuSakuru
+//
+//  Created by Greg-Mini on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct OtakuSakuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
