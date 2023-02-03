04f73767b5ba4facba126476deec5e8d68bc9bd5
GregDeveaux <deveauxgreg@gmail.com>
2023-02-04 00:03:03 +0100

    add logo Otaku

--- OtakuSakuru/ContentView.swift
+++ OtakuSakuru/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.red.ignoresSafeArea()
+            VStack {
+                Image("LogoOtakuSakuru")
+            }
+            .padding(50)
         }
-        .padding()
     }
 }
 
